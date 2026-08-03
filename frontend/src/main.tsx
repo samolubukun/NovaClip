8afbf001fc71306151dbf4c2714e33e98e2b5bd7
samolubukun <samuelolubukun@gmail.com>
2026-08-03 22:50:33 +0100

    Use persistent internal routing for clean browser URLs

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,21 +1,45 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { MemoryRouter, Route, Routes, useLocation } from "react-router-dom";
 import { Toaster } from "sonner";
 import "./styles/globals.css";
 import Nav from "./components/Nav";
 import Home from "./pages/Home";
 import Studio from "./pages/Studio";
+import NovaEdit from "./pages/NovaEdit";
 import TaskPage from "./pages/Task";
 import History from "./pages/History";
 
+const INTERNAL_ROUTE_KEY = "novaclip_internal_route";
+
+function RoutePersistence() {
+  const location = useLocation();
+
+  useEffect(() => {
+    sessionStorage.setItem(INTERNAL_ROUTE_KEY, `${location.pathname}${location.search}`);
+  }, [location.pathname, location.search]);
+
+  return null;
+}
+
+const browserRoute = `${window.location.pathname}${window.location.search}`;
+const initialRoute = browserRoute !== "/"
+  ? browserRoute
+  : sessionStorage.getItem(INTERNAL_ROUTE_KEY) || "/";
+
+if (window.location.pathname !== "/" || window.location.search) {
+  window.history.replaceState(null, "", "/");
+}
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <MemoryRouter initialEntries={[initialRoute]}>
+      <RoutePersistence />
       <Nav />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/studio" element={<Studio />} />
+        <Route path="/novaedit" element={<NovaEdit />} />
         <Route path="/task/:id" element={<TaskPage />} />
         <Route path="/history" element={<History />} />
       </Routes>
@@ -30,6 +54,6 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
           },
         }}
       />
-    </BrowserRouter>
+    </MemoryRouter>
   </React.StrictMode>
 );
